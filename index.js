32b733c94d320b24994255427047e50b58d87406
Hilarius L. Doren <larudoren@users.noreply.github.com>
2023-10-12 12:21:40 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function ascOrder(arr) {
+  for (let i = 0; i <= arr.length - 2; i++){
+    for (let j = i+1; j <= arr.length-1; j++ ){
+      if (arr[j] < arr[i]) {
+        let temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+      }
+    }
+  }
+  
+  return arr;
+}
+
+console.log(ascOrder([3, 5, 8, 4, 1, 9, -2]));
